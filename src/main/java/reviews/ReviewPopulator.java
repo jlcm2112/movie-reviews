f75fb0eca23d54002cab51191b1946952402f86f
Jamie Milhoan <jlmilhoan@gmail.com>
2017-07-12 13:35:29 -0400

    flexbox

--- src/main/java/reviews/ReviewPopulator.java
+++ src/main/java/reviews/ReviewPopulator.java
@@ -20,19 +20,21 @@ public class ReviewPopulator implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Tag dangerous = new Tag("dangerous");
+		Tag dangerous = new Tag("Dangerous");
 		tagRepo.save(dangerous);
-		
-		Tag romantic = new Tag("romantic");
+		Tag romantic = new Tag("Romantic");
 		tagRepo.save(romantic);
+		Tag superhero = new Tag("Super Hero");
+		tagRepo.save(superhero);
 		
-		Genre action = new Genre("action");
-		Genre romance = new Genre("romance");
+		Genre action = new Genre("Action");
+		Genre romance = new Genre("Romance");
 		genreRepo.save(action);
 		genreRepo.save(romance);
-		Review review1 = new Review (action, "Superman", "This is a good movie", "March 10, 2015", "Synopsis", "/images/superman.jpg", romantic);
+		
+		Review review1 = new Review (action, "Superman", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam lorem vitae metus tristique varius. Phasellus pulvinar finibus euismod. Phasellus dapibus magna ut purus viverra fermentum. Etiam pellentesque nulla sit amet ante faucibus porttitor eu a nisi. Cras eu lorem euismod, viverra ex nec, scelerisque erat. Maecenas risus urna, varius vitae auctor quis, placerat quis justo. Vestibulum sollicitudin cursus nisl, nec laoreet nibh pharetra et. Etiam commodo diam vel lectus dictum interdum. Cras tempus metus placerat, porta sapien sit amet, convallis turpis. Ut vel quam luctus, laoreet quam et, sagittis ante. Mauris in justo ac justo laoreet consequat sed id magna. Duis ipsum erat, ornare quis volutpat eu, vestibulum et tellus.", "March 10, 2015", "Synopsis", "/images/superman.jpg", superhero);
 		reviewRepo.save(review1);
-		Review review2 = new Review (action, "Batman", "This is a good movie", "March 10, 2015", "Synopsis", "/images/batman.jpg", romantic);
+		Review review2 = new Review (action, "Batman", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam lorem vitae metus tristique varius. Phasellus pulvinar finibus euismod. Phasellus dapibus magna ut purus viverra fermentum. Etiam pellentesque nulla sit amet ante faucibus porttitor eu a nisi. Cras eu lorem euismod, viverra ex nec, scelerisque erat. Maecenas risus urna, varius vitae auctor quis, placerat quis justo. Vestibulum sollicitudin cursus nisl, nec laoreet nibh pharetra et. Etiam commodo diam vel lectus dictum interdum. Cras tempus metus placerat, porta sapien sit amet, convallis turpis. Ut vel quam luctus, laoreet quam et, sagittis ante. Mauris in justo ac justo laoreet consequat sed id magna. Duis ipsum erat, ornare quis volutpat eu, vestibulum et tellus.", "March 10, 2015", "Synopsis", "/images/batman.jpg", superhero);
 		reviewRepo.save(review2);
 
 
